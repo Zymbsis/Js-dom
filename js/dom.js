56a5a59ee42bcd4bd59c39cf7cbbf1154c725199
Zymbsis <Aleksandra.aleksashka1809@gmail.com>
2023-12-30 18:27:56 +0100

    test

---
+++ js/dom.js
@@ -0,0 +1,21 @@
+const _ = require('lodash');
+
+//TODO === textContent ======
+// console.log(
+//   document
+//     .querySelector('.employee')
+//     .textContent.replace(/\s{2,}/g, '\n')
+//     .trim(),
+// );
+// Mark Guerrero
+// Product designer
+// console.log(document.querySelector('.our-team-subtitle').textContent);
+// Mark Guerrero
+// console.log(document.querySelector('.our-team-content').textContent);
+// Product designer
+
+console.log(_.sum([4, 2, 8, 6])); // 20
+console.log(_.sum([5, 10])); // 15
+
+console.log(_.shuffle([1, 2, 3, 4])); // [4, 1, 3, 2]
+console.log(_.shuffle([1, 2, 3, 4])); // [3, 2, 1, 4]
